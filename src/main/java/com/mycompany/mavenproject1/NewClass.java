9c0f203f523d2770b9b09d980d27b1873a60fc8e
Vasqjr <93558391+Vasqjr@users.noreply.github.com>
2023-01-30 17:01:23 -0500

    Worked on the NewClass setters, getters, constructor.

---
+++ src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Owner
+ */
+public class NewClass {
+    
+    private int age;
+    
+    public NewClass() {
+        this.age = 0;
+    }
+    
+    public NewClass(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+    
+}
